f9d5629ea6b35fc4f90a28cddb35fbbeef8d6df5
Dariana Gómez <darigomez30@gmail.com>
2020-11-26 22:35:03 -0300

    Subida de desafio

---
+++ assets/js/custom.js
@@ -0,0 +1,17 @@
+$(document).ready(function(){
+
+    //Smooth Scroll
+        $("a").click(function(event){
+          if (this.hash !== "") {
+            event.preventDefault();
+
+            var suave = this.hash;
+
+            $("html, body").animate({
+              scrollTop: $(suave).offset().top
+            }, 900, function(){
+              window.location.hash = suave;
+            });
+          }
+        });
+      });
